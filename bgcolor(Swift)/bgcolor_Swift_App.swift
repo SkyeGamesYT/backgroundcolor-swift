75292aa368ae348e16bc71fb95d3c260acdeedb6
SkyeGamesYT <oooyayt@gmail.com>
2023-03-10 08:08:31 -0600

    Initial Commit

---
+++ bgcolor(Swift)/bgcolor_Swift_App.swift
@@ -0,0 +1,17 @@
+//
+//  bgcolor_Swift_App.swift
+//  bgcolor(Swift)
+//
+//  Created by Gaskell, Roydon (514077) on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct bgcolor_Swift_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
